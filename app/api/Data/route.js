22b9fe1b522ae2ad6d2b7906ca521039e0e5b366
Logan <ltao47@gatech.edu>
2024-03-19 21:53:52 -0400

    Now displays unique names on front page

--- app/api/Data/route.js
+++ app/api/Data/route.js
@@ -1,15 +1,27 @@
 import values from "../../(models)/Value";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    const value = await values.find();
+    console.log(value);
+    console.log("FJIOWEFJOIWEFJIWOEFJIOWEFJWEIOJIOWFJWEIOFJWEIOF");
+    return NextResponse.json({ value }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
+
 export async function POST(req) {
   try {
     const body = await req.json();
     const theData = body.formData;
 
     await values.create(theData);
-    console.log(theData);
+    // console.log(theData);
     return NextResponse.json({ message: "Data Created" }, { status: 201 });
   } catch (err) {
-    return NextResponse.json({ message: "Error", error }, { status: 500 });
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,39 @@
-import EntryCard from "./(components)/EntryCard";
+import React from "react";
+import values from "./(models)/Value";
 
-const Dashboard = () => {
+async function HomePage({ uniqueNames }) {
+  const names = await getNames();
   return (
     <div>
-      <EntryCard />
+      <h1>Unique Names</h1>
+      <ul>
+        {names.uniqueNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
-export default Dashboard;
+export async function getNames() {
+  try {
+    const data = await values.find();
+    // console.log(data);
+    // console.log("FJIOWEFJOIWEFJIWOEFJIOWEFJWEIOJIOWFJWEIOFJWEIOF");
+
+    // get unique names out of the data
+    const uniqueNames = data ? [...new Set(data.map((item) => item.name))] : [];
+    console.log(uniqueNames);
+    return {
+      // props: { uniqueNames },
+      uniqueNames,
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: { error: "Error fetching data" },
+    };
+  }
+}
+
+export default HomePage;
