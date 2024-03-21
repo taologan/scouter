a13009a61ba198ef8f08c14034eafa5439d36562
Logan <ltao47@gatech.edu>
2024-03-21 18:24:59 -0400

    Update main page

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import values from "./(models)/Value";
 // const fs = require("fs");
 
 const HomePage = async () => {
+  
   const names = await getNames();
   // handleDownload();
   return (
@@ -28,9 +29,66 @@ const HomePage = async () => {
   );
 }
 
+// const handleDownload = async () => {
+//   const data = await values.find();
+//   console.log(data);
+//   const csvStream = fastcsv.format({ headers: true });
+//   const writableStream = fs.createWriteStream('app/documents/out.csv');
+  
+//   csvStream.pipe(writableStream);
+   
+//   data.forEach(entry => {
+//     csvStream.write({
+//       _id: entry._id.toString(),
+//       name: entry.name,
+//       matchNumber: entry.matchNumber,
+//       position: entry.position,
+//       noShow: entry.noShow,
+//       mobility: entry.mobility,
+//       ampScoredAuto: entry.ampScoredAuto,
+//       speakerScoredAuto: entry.speakerScoredAuto,
+//       ampScoredTeleop: entry.ampScoredTeleop,
+//       speakerScoredTeleop: entry.speakerScoredTeleop,
+//       speakerDefense: entry.speakerDefense,
+//       sourceDefense: entry.sourceDefense,
+//       trap: entry.trap,
+//       endPosition: entry.endPosition,
+//       disabled: entry.disabled,
+//       foul: entry.foul,
+//       totalScore: entry.totalScore,
+//       additionalComments: entry.additionalComments,
+//       createdAt: entry.createdAt,
+//       updatedAt: entry.updatedAt,
+//       __v: entry.__v
+//     });
+//   });
+//   console.log("Done!");
+// }
 HomePage.getInitialProps = async () => {
   const names = await getNames();
   return { names };
+}
+
+const getNames = async() =>  {
+  try {
+    
+    const data = await values.find();
+    // console.log(data);
+    // console.log("FJIOWEFJOIWEFJIWOEFJIOWEFJWEIOJIOWFJWEIOFJWEIOF");
+
+    // get unique names out of the data
+    const uniqueNames = data ? [...new Set(data.map((item) => item.name))] : [];
+    // console.log(uniqueNames);
+    return {
+      // props: { uniqueNames },
+      uniqueNames,
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: { error: "Error fetching data" },
+    };
+  }
 }
 
 export default HomePage;
