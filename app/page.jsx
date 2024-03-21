17acf270e5ac997001c8f076530190d15f60c344
Logan <ltao47@gatech.edu>
2024-03-21 17:07:15 -0400

    Revert Testing

--- app/page.jsx
+++ app/page.jsx
@@ -9,30 +9,24 @@ const HomePage = async () => {
   const names = await getNames();
   return (
     <div>
-      <Link href="/teams"><p>Teams</p></Link>
-      
+      <div>
+        {/* <h1>Download CSV</h1> */}
+        {/* <DownloadCSV /> */}
+        {/* <button onClick={handleDownload}>Download</button> */}
+        <br />
+      </div>
+      <h1>Teams</h1>
+      <ul>
+        {names.uniqueNames.map((name, index) => (
+          <li key={index}>
+            <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+              <a href={`/teamPage/${name}`}>{name}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-  // return (
-  //   <div>
-  //     <div>
-  //       {/* <h1>Download CSV</h1> */}
-  //       {/* <DownloadCSV /> */}
-  //       {/* <button onClick={handleDownload}>Download</button> */}
-  //       <br />
-  //     </div>
-  //     <h1>Teams</h1>
-  //     <ul>
-  //       {names.uniqueNames.map((name, index) => (
-  //         <li key={index}>
-  //           <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
-  //             <a href={`/teamPage/${name}`}>{name}</a>
-  //           </div>
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
+  );
 }
 
 const handleDownload = async () => {
